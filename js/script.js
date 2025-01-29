ebe80e01b39b3f361dedb7639635efac3eaa1796
SerheyBr <sergeydrilkov@mail.ru>
2025-01-29 13:22:10 +0300

    add action tab and carusel

--- js/script.js
+++ js/script.js
@@ -21,10 +21,59 @@ burgrerMenuBtnOpen.addEventListener('click', (e) => {
 	closeBurgerMenu()
 })
 
-//dropdown
-const header = document.querySelector('.header')
+//tabs
+const partnersTabsButtons = document.querySelectorAll('.partners-tabs__tab')
+const partnersTabsContent = document.querySelectorAll('.partners-tabs__content')
 
-header.addEventListener('click', (e) => {
-	console.log(e)
+const removeActiveClasses = () => {
+	partnersTabsButtons.forEach((el) => el.classList.remove('active'))
+	partnersTabsContent.forEach((el) => el.classList.remove('active'))
+}
+
+const setActiveTab = (elemen, contentId) => {
+	elemen.classList.add('active')
+
+	const content = document.getElementById(contentId)
+
+	if(content){
+		content.classList.add('active')
+	}
+}
+
+partnersTabsButtons.forEach((el) => {
+	el.addEventListener('click', (e) => {
+		const tab = e.currentTarget
+		const tabId = tab.dataset.tab
+		
+		removeActiveClasses()
+		setActiveTab(tab, tabId)
+	})
 })
 
+//acordeon
+const questionsAccordionItems = document.querySelectorAll('.questions-accordion__item')
+
+const closeAll = () => {
+	questionsAccordionItems.forEach((el) => {
+		el.classList.remove('active')
+	})
+}
+
+const openSelectedItem = (tab) => {
+	closeAll()
+	tab.classList.toggle('active')
+}
+
+const closeSelectedItem = (tab) => {
+	tab.classList.toggle('active')
+}
+
+questionsAccordionItems.forEach((el) => {
+	el.addEventListener('click', (e) => {	
+		if(e.currentTarget.classList.contains('active')){
+			closeSelectedItem(e.currentTarget)
+		}else{
+			openSelectedItem(e.currentTarget)
+		}
+	})
+})
