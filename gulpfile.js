7f8dfff75b8e2d5eda8fdbae09329f1dd477d991
SerheyBr <sergeydrilkov@mail.ru>
2025-02-06 18:24:05 +0300

    add srvices page

--- gulpfile.js
+++ gulpfile.js
@@ -92,7 +92,7 @@ export function cleanDist() {
 export function browsersync() {
    browserSync.init({
       server: { baseDir: paths.dist.html },
-      startPath: '/about.html',
+      startPath: '/services.html',
       notify: false,
    })
 }
